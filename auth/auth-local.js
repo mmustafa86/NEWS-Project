286852cd7fbd403eca81a9c58773b2539788084e
mohammed mustafa <mohammed.alkhddar@gmail.com>
2020-01-11 00:57:32 -0600

    finished signup

--- auth/auth-local.js
+++ auth/auth-local.js
@@ -4,8 +4,9 @@ const session = require('express-session');
 const passport =require('passport');
 var router = express.Router();
 const models= require('../models');
+const bodyParser =require('body-parser')
 
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(session({
     secret: "user_id", 
@@ -15,10 +16,54 @@ app.use(session({
   app.use(passport.initialize());
   app.use(passport.session());
 
+  //encrypting function 
+  var pbkdf2 = require('pbkdf2');
+  var salt =process.env.SALT_KEY ;
+
+  function encryptionPassword(password){
+    var key =pbkdf2.pbkdf2Sync(
+      password,salt, 36000,256, 'sha256'
+    );
+    var hash= key.toString('hex')
+    return hash;
+  }
+// passport local 
+
+  const LocalStrategy = require('passport-local').Strategy
+
+ // Register User
 
 
-
-
+ router.post("/signup", function (req, res) {
+    models.accounts.findOne({
+    where: {
+      username: req.body.username
+    }}).then(function(user){
+      console.log(user)
+      if(!user){
+        models.accounts.create({ 
+          firstname: req.body.firstname,
+           lastname: req.body.lastname,
+           username: req.body.username,
+           email: req.body.email,
+           password: encryptionPassword(req.body.password),
+           
   
+      }).error(function(err){
+        console.log(err);
+        res.render('signup', {error: 'The user is already created '})
+      });
+      } else {
+      res.render('signup', {error: 'The user is already created '})
+      }
+      res.redirect("signin")
+    })
+    
+    });
+
+
+
+
+
 
   module.exports = router
