52c4c99939617930c6c7ad541e316f33a179a886
mohammed mustafa <mohammed.alkhddar@gmail.com>
2020-01-11 13:15:14 -0600

    finish the api call

--- route/route.js
+++ route/route.js
@@ -5,6 +5,8 @@ const passport =require('passport');
 var router = express.Router();
 const models= require('../models');
 
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('7f830f70a9b541b9bb7957578e96b91c')
 
 app.use(session({
     secret: "user_id", 
@@ -15,28 +17,39 @@ app.use(session({
   app.use(passport.initialize());
   app.use(passport.session());
 
+
+
   router.get('/',function(req,res){
+      res.render('main.ejs')
+  })
 
 
-    // newsapi.v2.topHeadlines({
-    //     q: 'trump',
-    //     category: 'politics',
-    //     language: 'en',
-    //     country: 'us'
-    //   }).then(response => {
-    //     //   var data=JSON.stringify(response);
-    //    res.json(response)
-    //     /*
-    //       {
-    //         status: "ok",
-    //         articles: [...]
-    //       }
-    //     */
-    //   }).catch(function(error){
-    //       console.log(error)
-    //   })
-    res.render('main.ejs')
+
+  router.get('/news/:id',function(req,res){
+
+
+    newsapi.v2.sources({
+        category: req.params.category,
+        language: 'en',
+        country: 'us'
+      }).then(response => {
+        console.log(response);
+        //   var data=JSON.stringify(response);
+       res.json(response)
+        /*
+          {
+            status: "ok",
+            articles: [...]
+          }
+        */
+      }).catch(function(error){
+          console.log(error)
+      })
+    
     })
+
+
+
     router.get('/signin',function(req,res){
 
         res.render('signin.ejs')
